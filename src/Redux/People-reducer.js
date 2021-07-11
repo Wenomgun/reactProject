7d73725fa9e612c2a63b9d12322eb50a2183ed36
Ilya Kuzmin <akalyig@mail.ru>
2021-07-11 11:02:56 +0300

    class component people, get query, paging

--- src/Redux/People-reducer.js
+++ src/Redux/People-reducer.js
@@ -1,26 +1,36 @@
 const CHANGE_FOLLOW = 'changeFollowed';
 const SHOW_MORE = 'showMore';
 const SET_PEOPLE = 'setPeople';
+const SET_TOTAL_PEOPLE = 'setTotalPeople';
+const SET_CURRENT_PAGE = 'setCurrentPage';
 
-let initialPeople = [
+let initialState = {
+    peopleData: [],
+    pageSize: 3,
+    totalPeople: 0,
+    currentPage: 1
+};
 
-];
-
-const peopleReducer = (state = initialPeople, action) => {
+const peopleReducer = (state = initialState, action) => {
     if (action.type === CHANGE_FOLLOW) {
         let userId = action.data;
-        let newState = [...state];
-        newState = newState.map((user) => {
-            if (user.userId !== userId) {
-                return user;
-            }
-            return {...user, isFollowed: !user.isFollowed}
-        });
-        return newState;
+        return {
+            ...state,
+            peopleData: state.peopleData.map((user) => {
+                if (user.userId !== userId) {
+                    return user;
+                }
+                return {...user, isFollowed: !user.isFollowed}
+            })
+        };
     } else if (action.type === SHOW_MORE) {
-        return [...state, ...action.data];
+        return {...state, peopleData: [...state.peopleData, ...action.data]};
     } else if (action.type === SET_PEOPLE) {
-        return [...action.data];
+        return {...state, peopleData: [...action.data]};
+    } else if (action.type === SET_TOTAL_PEOPLE) {
+        return {...state, totalPeople: action.data};
+    } else if (action.type === SET_CURRENT_PAGE) {
+        return {...state, currentPage: action.data};
     }
     return state;
 }
@@ -38,6 +48,16 @@ export const setPeopleActionCreator = (people) => ({
 export const showMoreActionCreator = (people) => ({
     type: SHOW_MORE,
     data: people
+});
+
+export const setTotalPeople = (count) => ({
+    type: SET_TOTAL_PEOPLE,
+    data: count
+});
+
+export const setCurrentPage = (page) => ({
+    type: SET_CURRENT_PAGE,
+    data: page
 });
 
 export default peopleReducer;
--- src/components/People/PeopleContainer.jsx
+++ src/components/People/PeopleContainer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {changeFollowedActionCreator, setPeopleActionCreator, showMoreActionCreator} from "../../Redux/People-reducer";
+import {
+    changeFollowedActionCreator,
+    setCurrentPage,
+    setPeopleActionCreator,
+    setTotalPeople,
+    showMoreActionCreator
+} from "../../Redux/People-reducer";
 import People from "./People";
 
 const mapStateToProps = (state) => {
     return {
-        peopleData: state.peopleData
+        peopleData: state.peopleData.peopleData,
+        pageSize: state.peopleData.pageSize,
+        totalPeople: state.peopleData.totalPeople,
+        currentPage: state.peopleData.currentPage,
     }
 }
 
@@ -19,6 +28,12 @@ const mapDispatchToProps = (dispatch) => {
         },
         setPeople: (people) => {
             dispatch(setPeopleActionCreator(people));
+        },
+        setTotalPeople: (count) => {
+            dispatch(setTotalPeople(count));
+        },
+        setCurrentPage: (page) => {
+            dispatch(setCurrentPage(page));
         }
     }
 }
